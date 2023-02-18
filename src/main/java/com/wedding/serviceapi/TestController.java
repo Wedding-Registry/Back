119de71bdd84291bc5e99287c6422cc3c61a6afb
Jin-Mook <jink0608@naver.com>
2023-02-19 00:08:40 +0900

    [feature/WEDPRO-13] github webhook test

---
+++ src/main/java/com/wedding/serviceapi/TestController.java
@@ -0,0 +1,15 @@
+package com.wedding.serviceapi;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String webHookTest() {
+        return "jenkins test";
+    }
+}
