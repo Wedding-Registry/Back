ec86fdef278067d8b47270ce5c752458466e0344
jinmook jeong <91299082+Jin-Mook@users.noreply.github.com>
2023-03-01 03:17:59 +0900

    [WEDPRO-13] ci/cd feature에서 push할때 구축 완료
    * [feature/WEDPRO-13] github webhook test
    
    * [feature/WEDPRO-13] jenkins build test
    
    * [feature/WEDPRO-13] jenkins nohup jar log test
    
    * [feature/WEDPRO-13] jenkins webhook test
    
    * [feature/WEDPRO-13] docker-compose파일 작성
    
    * [feature/WEDPRO-13] Dockerfile 수정
    
    * [feature/WEDPRO-13] Dockerfile entrypoint 수정
    
    * [feature/WEDPRO-13] Dockerfile 권한 수정
    
    * [feature/WEDPRO-13] docker-compose 권한에러 테스트
    
    * [feature/WEDPRO-13] docker-compose 권한에러 테스트2
    
    * [feature/WEDPRO-13] docker-compose 권한에러 테스트3
    
    * [feature/WEDPRO-13] docker-compose 권한에러 테스트4
    
    * [feature/WEDPRO-13] docker-compose 권한에러 테스트5
    
    * [feature/WEDPRO-13] docker-compose 권한에러 테스트6
    
    * [feature/WEDPRO-13] docker-compose 권한에러 테스트7

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM openjdk:11-jdk
+
+ARG JAR_FILE=serviceapi-0.1.jar
+
+WORKDIR /app
+
+RUN chmod a+x /app
+
+COPY build/libs/${JAR_FILE} .
+
+ENTRYPOINT ["java", "-jar", "/app/serviceapi-0.1.jar", "--spring.profiles.active=prod"]
+
---
+++ src/main/java/com/wedding/serviceapi/TestController.java
@@ -0,0 +1,22 @@
+package com.wedding.serviceapi;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String webHookTest() {
+        log.info("nohup log test");
+        return "jenkins ci/cd and remove none image test";
+    }
+
+    @GetMapping("/test")
+    public String webHookTest2() {
+        log.info("webhooktest");
+        return "jenkins test2";
+    }
+}
