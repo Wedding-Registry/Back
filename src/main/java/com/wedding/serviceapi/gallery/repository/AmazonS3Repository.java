a2eee4cf46e66ca4b836826c25d74566267499da
Jin-Mook <jink0608@naver.com>
2023-05-23 02:17:10 +0900

    [feature/WEDPRO-68] inputStream 리소스 정리 코드 추가 및 테스트 코드 작성 완료

--- src/main/java/com/wedding/serviceapi/gallery/repository/AmazonS3Repository.java
+++ src/main/java/com/wedding/serviceapi/gallery/repository/AmazonS3Repository.java
@@ -26,20 +26,19 @@ public class AmazonS3Repository implements S3Repository {
     public String uploadObject(MultipartFile file, Long usersId) {
         // s3 객체에 저장하기
         // 파일 이름 변환 -> s3 key 값으로 userId-uuid 로 진행
-        InputStream inputStream;
-        try {
-            inputStream = file.getInputStream();
+        String galleryImgUrl;
+        try (InputStream inputStream = file.getInputStream()) {
+            String key = usersId + "-" + UUID.randomUUID();
+            galleryImgUrl = s3Url + key;
+
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentType(file.getContentType());
+            objectMetadata.setContentLength(file.getSize());
+            amazonS3.putObject(BUCKET_NAME, key, inputStream, objectMetadata);
         } catch (IOException e) {
             throw new IllegalArgumentException("잘못된 파일입니다.");
         }
 
-        String key = usersId + "-" + UUID.randomUUID();
-        String galleryImgUrl = s3Url + key;
-
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-        objectMetadata.setContentType(file.getContentType());
-        objectMetadata.setContentLength(file.getSize());
-        amazonS3.putObject(BUCKET_NAME, key, inputStream, objectMetadata);
         return galleryImgUrl;
     }
 
