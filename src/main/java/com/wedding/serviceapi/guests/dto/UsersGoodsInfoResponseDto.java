94f1053a6d81d5267ecf64538fec4d819a37575b
Jin-Mook <jink0608@naver.com>
2023-07-05 22:31:06 +0900

    [feature/WEDPRO-80] 상품 후원 api 개발 및 테스트 코드 작성 완료

--- src/main/java/com/wedding/serviceapi/guests/dto/UsersGoodsInfoResponseDto.java
+++ src/main/java/com/wedding/serviceapi/guests/dto/UsersGoodsInfoResponseDto.java
@@ -1,13 +1,13 @@
-package com.wedding.serviceapi.goods.dto;
+package com.wedding.serviceapi.guests.dto;
 
 import com.wedding.serviceapi.goods.domain.UsersGoods;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
-@Getter @Setter
+@Getter
 @ToString
-public class UsersGoodsInfoDto {
+public class UsersGoodsInfoResponseDto {
     private Long usersGoodsId;
     private String usersGoodsName;
     private Integer usersGoodsPrice;
@@ -15,13 +15,18 @@ public class UsersGoodsInfoDto {
     private Integer usersGoodsPercent;
     private String usersGoodsImgUrl;
 
-    public UsersGoodsInfoDto(UsersGoods usersGoods) {
+    @Builder
+    private UsersGoodsInfoResponseDto(UsersGoods usersGoods) {
         this.usersGoodsId = usersGoods.getId();
         this.usersGoodsName = usersGoods.getUpdatedUsersGoodsName();
         this.usersGoodsPrice = usersGoods.getUpdatedUsersGoodsPrice();
         this.usersGoodsTotalDonation = usersGoods.getUsersGoodsTotalDonation();
         this.usersGoodsPercent = calculateDonationPercent(usersGoods.getUpdatedUsersGoodsPrice(), usersGoods.getUsersGoodsTotalDonation());
         this.usersGoodsImgUrl = usersGoods.getGoods().getGoodsImgUrl();
+    }
+
+    public static UsersGoodsInfoResponseDto from(UsersGoods usersGoods) {
+        return new UsersGoodsInfoResponseDto(usersGoods);
     }
 
     private Integer calculateDonationPercent(Integer usersGoodsPrice, Integer usersGoodsTotalDonation) {
--- src/main/java/com/wedding/serviceapi/guests/service/InvitationService.java
+++ src/main/java/com/wedding/serviceapi/guests/service/InvitationService.java
@@ -2,10 +2,14 @@ package com.wedding.serviceapi.guests.service;
 
 import com.wedding.serviceapi.boards.dto.weddinghall.WeddingHallInfoDto;
 import com.wedding.serviceapi.boards.service.WeddingHallService;
+import com.wedding.serviceapi.exception.NoBoardsIdCookieExistException;
+import com.wedding.serviceapi.goods.domain.UsersGoods;
 import com.wedding.serviceapi.goods.dto.UsersGoodsInfoDto;
+import com.wedding.serviceapi.goods.repository.UsersGoodsRepository;
 import com.wedding.serviceapi.goods.service.UsersGoodsService;
 import com.wedding.serviceapi.guests.domain.AttendanceType;
 import com.wedding.serviceapi.guests.domain.Guests;
+import com.wedding.serviceapi.guests.dto.UsersGoodsInfoResponseDto;
 import com.wedding.serviceapi.guests.invitationinfo.InvitationInfoSetter;
 import com.wedding.serviceapi.gallery.dto.S3ImgInfoDto;
 import com.wedding.serviceapi.gallery.service.GalleryService;
@@ -30,6 +34,7 @@ public class InvitationService {
     private final WeddingHallService weddingHallService;
     private final InvitationInfoSetter invitationInfoSetter;
     private final GuestsRepository guestsRepository;
+    private final UsersGoodsRepository usersGoodsRepository;
 
     public List<S3ImgInfoDto> findAllGalleryImg(HttpServletRequest request, HttpServletResponse response, Long usersId) {
         invitationInfoSetter.checkInvitationInfoAndSettingInfoIfNotExist(request, response, usersId);
@@ -55,5 +60,12 @@ public class InvitationService {
         // TODO: 2023/06/17 gusetsRepository를 쿠키 체크 부분에서도 이용하게 된다 -> 로직 수정 해보자
         Guests guests = guestsRepository.findByUsersIdAndBoardsId(usersId, boardsId).orElseThrow(() -> new NoSuchElementException("해당 사용자는 초대되지 않았습니다."));
         guests.changeAttendanceType(attendanceType);
+    }
+
+    public UsersGoodsInfoResponseDto donateUsersGoods(HttpServletRequest request, HttpServletResponse response, Long usersGoodsId, int donation, Long usersId) {
+        invitationInfoSetter.checkInvitationInfoAndSettingInfoIfNotExist(request, response, usersId);
+        UsersGoods usersGoods = usersGoodsRepository.findById(usersGoodsId).orElseThrow(() -> new NoSuchElementException("해당하는 상품 항목이 없습니다."));
+        usersGoods.donateMoney(donation);
+        return UsersGoodsInfoResponseDto.from(usersGoods);
     }
 }
