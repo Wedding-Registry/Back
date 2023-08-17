6953c9bd6a85ce372673eb66496462421cf6f416
Jin-Mook <jink0608@naver.com>
2023-08-17 15:31:55 +0900

    [feature/WEDPRO-114] 네비게이션 알람 목록 5개로 제한하도록 쿼리 수정 완료

--- src/main/java/com/wedding/serviceapi/guests/repository/GoodsDonationRepository.java
+++ src/main/java/com/wedding/serviceapi/guests/repository/GoodsDonationRepository.java
@@ -1,6 +1,7 @@
 package com.wedding.serviceapi.guests.repository;
 
 import com.wedding.serviceapi.guests.domain.GoodsDonation;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,10 +10,15 @@ import java.util.List;
 import java.util.Optional;
 
 public interface GoodsDonationRepository extends JpaRepository<GoodsDonation, Long> {
+    // TODO: 2023/08/17 쿼리 리팩토링 필요
 
     @Query("select gd from GoodsDonation gd join fetch gd.guests g join fetch g.users u join fetch gd.usersGoods" +
             " where g.boards.id = :boardsId order by gd.updatedAt desc")
     List<GoodsDonation> findAllByBoardsIdWithUser(@Param("boardsId") Long boardsId);
+
+    @Query("select gd from GoodsDonation gd join fetch gd.guests g join fetch g.users u join fetch gd.usersGoods" +
+            " where g.boards.id = :boardsId order by gd.updatedAt desc")
+    List<GoodsDonation> findLimit5ByBoardsIdWithUser(@Param("boardsId") Long boardsId, Pageable pageable);
 
     Optional<GoodsDonation> findByGuestsIdAndUsersGoodsId(Long guestsId, Long usersGoodsId);
 }
